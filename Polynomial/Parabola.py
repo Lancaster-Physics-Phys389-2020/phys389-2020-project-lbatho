c6a9964d104c5526027a16902482a57375f33dea
lbatho <l.batho@lancaster.ac.uk>
2020-03-06 23:42:16 +0000

    Workspace cleanup 2

--- Polynomial/Parabola.py
+++
@@ -1,8 +0,0 @@
-from Polynomial import Polynomial
-
-class Parabola(Polynomial):
-
-    def __init__(self, a,b,c):
-        # Polynomial ax^2 + bx +c 
-        # Note Polynomial maps constant as first item  
-        super().__init__([c,b,a])
